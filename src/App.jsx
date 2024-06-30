18814c235b7efdd42644b2d505efb60e6e7a7389
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-06-30 23:03:08 +0300

    fix task 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Profile from '../src/components/profile/Profile'
 import userData from './userData.json'
 import FriendsList from "./components/friends/Friends";
 import friends from "./FriendsList.json"
+import transactionData from './transactionData.json'
+import Transaction from '../src/components/transaction/Transaction'
 
 // import { useState } from 'react'
 import './App.css'
@@ -19,6 +21,7 @@ function App() {
       />
       <FriendsList friends={friends}
       />
+<Transaction items={transactionData}/>
     </>
   );
 }
